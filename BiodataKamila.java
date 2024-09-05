8ad3b72746e4bb361d5460b501384136457288e7
kmilaz14 <kamilazhwa@gmail.com>
2024-09-05 11:53:27 +0800

    commit biodata java Kamila

---
+++ BiodataKamila.java
@@ -0,0 +1,19 @@
+/**
+ * TugasJobSheet
+ */
+public class BiodataKamila {
+
+    public static void main(String[] args) {
+        System.out.println("-".repeat(60));
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("-".repeat(60));
+        System.out.println("Nama    : Kamila Zahwa");
+        System.out.println("NIM     : 244107020111");
+        System.out.println("Kelas   : 1D");
+        System.out.println("No Absen: 14");
+        System.out.println("Prodi   : D-IV Teknik Informatika");
+        System.out.println("Alamat  : Jl. Bahari, Kota Banjarbaru, Kalimantan Selatan");
+        System.out.println("No HP   : 083150540964");
+        System.out.println("Hobi    : Menyanyi dan Membaca");
+    }
+}
